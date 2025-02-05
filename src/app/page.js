4705c10adcba5f4895310d8ce2976e9c19345727
Pradeep8253 <89512667+Pradeep8253@users.noreply.github.com>
2025-02-05 23:02:13 +0530

    testimonials section

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,18 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
+import Pricing from "./components/Pricing";
+import Faq from "./components/Faq";
+import Testimonials from "./components/Testimonials";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Features />
+      <Pricing />
+      <Faq />
+      <Testimonials />
     </>
   );
 }
